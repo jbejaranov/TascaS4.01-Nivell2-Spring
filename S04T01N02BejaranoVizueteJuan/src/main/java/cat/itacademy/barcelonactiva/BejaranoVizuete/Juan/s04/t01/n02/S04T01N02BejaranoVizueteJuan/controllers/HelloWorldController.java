3c4e25a96b1cc0303e785a34524a5a055be801d5
Juan A. Bejarano <100026204+jbejaranov@users.noreply.github.com>
2022-09-27 09:29:15 +0200

    Update HelloWorldController.java

--- S04T01N02BejaranoVizueteJuan/src/main/java/cat/itacademy/barcelonactiva/BejaranoVizuete/Juan/s04/t01/n02/S04T01N02BejaranoVizueteJuan/controllers/HelloWorldController.java
+++ S04T01N02BejaranoVizueteJuan/src/main/java/cat/itacademy/barcelonactiva/BejaranoVizuete/Juan/s04/t01/n02/S04T01N02BejaranoVizueteJuan/controllers/HelloWorldController.java
@@ -11,13 +11,13 @@ public class HelloWorldController {
 	@GetMapping("/HelloWorld")
 	String saluda(@RequestParam(defaultValue = "UNKNOWN") String nom) {
 
-		return "Hola, " + nom + ". Estàs executant un projecte Maven.";
+		return "Hola, " + nom + ". Estàs executant un projecte Gradle.";
 	}
 
 	@GetMapping(value = { "/HelloWorld2", "/HelloWorld2/{nom}" })
 	String saluda2(@PathVariable(required = false) String nom) {
 
-		return "Hola, " + nom + ". Estàs executant un projecte Maven.";
+		return "Hola, " + nom + ". Estàs executant un projecte Gradle.";
 	}
 
 }
